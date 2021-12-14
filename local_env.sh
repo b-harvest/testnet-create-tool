54dafda7331fd2c1e4ca563f4ae512134770465b
jason song <jason.song@bharvest.io>
2021-12-14 12:57:32 +0900

    rediect for gaiad, iris

--- local_env.sh
+++ local_env.sh
@@ -213,14 +213,14 @@ rm MNEMONIC.key
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46
 do
-        cli=$(gaiad keys add hermes$i --keyring-backend test --output json | jq -r '.')
+        cli=$(gaiad keys add hermes$i --keyring-backend test --output json 2>&1 | jq -r '.')
         MNEMONIC=$(echo $cli | jq -r '.mnemonic')
         echo -n "\"$MNEMONIC\"," >> MNEMONIC.key
         gaiada=$(echo $cli | jq -r '.address')
         bandda=$(echo "$MNEMONIC" | bandd keys add hermes$i --recover --keyring-backend test --output json| jq -r '.address')
         terrada=$(echo "$MNEMONIC" | terrad keys add hermes$i --recover --keyring-backend test --output json| jq -r '.address')
         osmosisda=$(echo "$MNEMONIC" | osmosisd keys add hermes$i --recover --keyring-backend test --output json| jq -r '.address')
-        irisa=$(echo "$MNEMONIC" | iris keys add hermes$i --recover --keyring-backend test --output json| jq -r '.address')
+        irisa=$(echo "$MNEMONIC" | iris keys add hermes$i --recover --keyring-backend test --output json 2>&1 | jq -r '.address')
         sentinelhuba=$(echo "$MNEMONIC" | sentinelhub keys add hermes$i --recover --keyring-backend test --output json| jq -r '.address')
         regena=$(echo "$MNEMONIC" | regen keys add hermes$i --recover --keyring-backend test --output json| jq -r '.address')
         akasha=$(echo "$MNEMONIC" | akash keys add hermes$i --recover --keyring-backend test --output json| jq -r '.address')
